1543736991ea2f5de0bdd7ac8f0b7d7229ed1142
Kemal <kemalbaylan@gmail.com>
2023-08-29 13:34:27 -0400

    Warehousecontroller added

---
+++ app/Http/Controllers/WarehouseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WarehouseController extends Controller
+{
+    //
+
+    /**
+     * Show the profile for a given user.
+     */
+
+}
